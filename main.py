0b9585a1988288c23944547536a64ce030648175
nsp4746 <nsp4746@rit.edu>
2024-11-04 10:38:53 -0500

    needs docx

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 import string, random
 FILE_PATH = 'weapons.txt'
 
+RARITIES = ["Common","Uncommon","Rare","Epic","Legendary","Mythic","Exotic"]
+
 def extract_weapons_from_txt():
     weapon_list = list()
     current_weapon = None
@@ -12,11 +14,10 @@ def extract_weapons_from_txt():
         for line in file:
             line = line.strip()
 
-            if line and not any(char.isdigit() for char in line) and line not in ["Common", "Uncommon", "Rare", "Epic",
-                                                                                  "Legendary", "Mythic"]:
+            if line and not any(char.isdigit() for char in line) and line not in RARITIES:
                 current_weapon = line
 
-            elif line in ["Common", "Uncommon", "Rare", "Epic", "Legendary", "Mythic"]:
+            elif line in RARITIES:
                 if current_weapon:
                     weapon_entry = f"{line} {current_weapon}"
                     weapon_list.append(weapon_entry)
@@ -59,10 +60,23 @@ def encode_string():
             encoded_output.append(weapon)
 
     print("Encoded Output:", encoded_output)
+    return encoded_output
 
+def decode_string(encoded_string=None):
+    decoded_output = []
+    weapon_dictionary = create_dictionary()
+    if encoded_string is None:
+        encoded_string = input("Enter your encoded string:")
+    else:
+        for weapon in encoded_string:
+            decoded_output.append(weapon_dictionary[weapon])
+
+    print("Decoded Output:", decoded_output)
+    return decoded_output
 
 def main():
-    encode_string()
+    encoded_string = encode_string()
+    decode_string(encoded_string)
 
 if __name__ == '__main__':
     main()
